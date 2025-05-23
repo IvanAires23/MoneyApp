80326f42e1bd9e431691428eeb9a37bae44e99b3
IvanAires23 <ivanaires32@gmail.com>
2025-05-23 09:37:03 -0300

    feat: date and category

--- app/(tabs)/add-transactions.jsx
+++ app/(tabs)/add-transactions.jsx
@@ -1,6 +1,10 @@
+import RNDateTimePicker from "@react-native-community/datetimepicker";
+import { Picker } from "@react-native-picker/picker";
 import { useState } from "react";
-import { Alert, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import { Alert, Platform, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import Button from "../../components/Button";
+import { categories } from "../../constants/categories";
+import { colors } from "../../constants/colors";
 import { globalStyles } from "../../styles/globalStyles";
 
 export default function AddTransaction() {
@@ -8,21 +12,30 @@ export default function AddTransaction() {
     const initialForm = {
         description: "",
         value: 0,
-        date: "",
+        date: new Date(),
         category: "Renda"
     }
 
     const [form, setForm] = useState(initialForm)
+    const [showPicker, setShowPicker] = useState(false)
 
     const addTransaction = () => {
         Alert.alert("Bom dia")
+    }
+
+    const handleDateChange = (e, selectDate) => {
+        setShowPicker(false)
+
+        if (selectDate) {
+            setForm({ ...form, date: selectDate })
+        }
     }
 
     const handleCurrencyChange = (text) => {
         const formattedValue = text.replace(/\D/g, "")
         const numberValue = formattedValue ? parseFloat(formattedValue) / 100 : 0;
 
-        setForm({ ...text, value: numberValue })
+        setForm({ ...form, value: numberValue })
     }
 
     return (
@@ -50,19 +63,61 @@ export default function AddTransaction() {
                     </View>
 
                     <View>
-                        <Text>Data</Text>
-                        <TextInput
-                            value={form.date}
-                            onChangeText={e => setForm({ ...form, date: e })}
-                            style={globalStyles.input} />
+                        <Text style={globalStyles.inputLabel}>Data</Text>
+                        <TouchableOpacity onPress={() => setShowPicker(true)}>
+                            <TextInput
+                                value={form.date.toLocaleDateString()}
+                                onChangeText={e => setForm({ ...form, date: e })}
+                                style={globalStyles.input}
+                                editable={false}
+                            />
+
+                            {showPicker && (
+                                <RNDateTimePicker
+                                    mode="date"
+                                    display={Platform.OS == "ios" ? "inline" : "default"}
+                                    value={form.date}
+                                    onChange={handleDateChange}
+                                />
+                            )
+                            }
+                        </TouchableOpacity>
+
                     </View>
 
                     <View>
                         <Text>Categoria</Text>
-                        <TextInput
+                        {/*<TextInput
                             value={form.category}
                             onChangeText={e => setForm({ ...form, category: e })}
-                            style={globalStyles.input} />
+                            style={globalStyles.input} />*/}
+                        <View style={style.picker}>
+                            <Picker
+                                selectedValue={form.category}
+                                onValueChange={itemValue => setForm({ ...form, category: itemValue })}
+                            >
+                                <Picker.Item
+                                    label={categories.income.displayName}
+                                    value={categories.income.name}
+                                />
+                                <Picker.Item
+                                    label={categories.food.displayName}
+                                    value={categories.food.name}
+                                />
+                                <Picker.Item
+                                    label={categories.education.displayName}
+                                    value={categories.education.name}
+                                />
+                                <Picker.Item
+                                    label={categories.house.displayName}
+                                    value={categories.house.name}
+                                />
+                                <Picker.Item
+                                    label={categories.travel.displayName}
+                                    value={categories.travel.name}
+                                />
+                            </Picker>
+                        </View>
                     </View>
                 </View>
 
@@ -78,5 +133,13 @@ const style = StyleSheet.create({
         marginBottom: 40,
         marginTop: 10
     },
-
+    picker: {
+        display: "flex",
+        justifyContent: "center",
+        height: 44,
+        borderColor: colors.secondaryText,
+        borderWidth: 1,
+        borderRadius: 8,
+        flexGrow: 1
+    }
 })
